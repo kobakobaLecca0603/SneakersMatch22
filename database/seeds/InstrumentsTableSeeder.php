4dedf6330bcc9689d262bae2b8b70557a01467d6
kobakobaLecca0603 <lingtaixiaolin1@gmail.com>
2023-01-05 06:54:23 +0900

    first commit

---
+++ database/seeds/InstrumentsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class InstrumentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('instruments')->insert([
+                ['name' => 'NIKE '],
+                ['name' => 'adidas '],
+                ['name' => 'VANS '],
+                ['name' => 'new balance '],
+                ['name' => 'Reebok '],
+                ['name' => 'CONVERSE '],
+                ['name' => 'PUMA '],
+                ['name' => 'Onitsuka Tiger '],
+                ['name' => 'Asics '],
+                ['name' => 'HOKA ONE ONE '],
+                ['name' => 'Balenciaga '],
+                ['name' => 'FILA '],
+                ['name' => 'Saucony '],
+                ['name' => 'LACOSTE '],
+            ]);
+    }
+}
